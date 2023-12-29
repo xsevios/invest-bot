5d3a4f35b68aa401f58fc711c7bfa11bd2e44a24
Nikita Lomov <nikitalom@gmail.com>
2023-12-29 22:46:01 +0300

    Fix multistrategy in __process_new_candles
    # Conflicts:
    #	trading/trader.py

--- trading/trader.py
+++ trading/trader.py
@@ -180,75 +180,80 @@ class Trader:
             strategies: dict[str, list[IStrategy]],
             signals_before_time: datetime
     ) -> None:
-        current_figi_candle = current_candles.setdefault(candle.figi, candle)
-        if candle.time < current_figi_candle.time:
-            # it happens (based on API documentation)
-            logger.debug("Skip candle from past.")
-            return
 
-        # check price from candle for take or stop price levels
-        current_trade_order = self.__today_trade_results.get_current_trade_order(candle.figi)
-        if current_trade_order:
-            high, low = quotation_to_decimal(candle.high), quotation_to_decimal(candle.low)
+        for strategy in strategies[candle.figi]:
+            current_figi_candle = current_candles.setdefault(candle.figi, candle)
+            if candle.time < current_figi_candle.time:
+                # it happens (based on API documentation)
+                logger.debug("Skip candle from past.")
+                return
 
-            # Logic is:
-            # if stop or take price level is between high and low, then stop or take will be executed
-            if low <= current_trade_order.signal.stop_loss_level <= high:
-                logger.info(f"STOP LOSS: {current_trade_order}")
-                close_order_id = \
-                    self.__close_position_by_figi(account_id, [candle.figi], strategies).get(candle.figi, None)
-                if close_order_id:
-                    trade_order = self.__today_trade_results.close_position(candle.figi, close_order_id)
-                    self.__blogger.close_position_message(trade_order)
+            # check price from candle for take or stop price levels
+            current_trade_order = self.__today_trade_results.get_current_trade_order(candle.figi)
+            if current_trade_order:
+                high, low = quotation_to_decimal(candle.high), quotation_to_decimal(candle.low)
 
-            elif low <= current_trade_order.signal.take_profit_level <= high:
-                logger.info(f"TAKE PROFIT: {current_trade_order}")
-                close_order_id = \
-                    self.__close_position_by_figi(account_id, [candle.figi], strategies).get(candle.figi, None)
-                if close_order_id:
-                    trade_order = self.__today_trade_results.close_position(candle.figi, close_order_id)
-                    self.__blogger.close_position_message(trade_order)
+                # Logic is:
+                # if stop or take price level is between high and low, then stop or take will be executed
+                if low <= current_trade_order.signal.stop_loss_level <= high:
+                    logger.info(f"STOP LOSS: {current_trade_order}")
+                    self.__blogger.send_text_message(f"STOP LOSS: {candle.figi}")
+                    # close_order_id = \
+                    #     self.__close_position_by_figi(account_id, [candle.figi], strategies).get(candle.figi, None)
+                    # if close_order_id:
+                    #     trade_order = self.__today_trade_results.close_position(candle.figi, close_order_id)
+                    #     self.__blogger.close_position_message(trade_order)
 
-        if candle.time > current_figi_candle.time and \
-                datetime.datetime.utcnow().replace(tzinfo=datetime.timezone.utc) <= signals_before_time:
-            signal_new = strategies[candle.figi].analyze_candles(
-                [candle_to_historiccandle(current_figi_candle)]
-            )
+                elif low <= current_trade_order.signal.take_profit_level <= high:
+                    logger.info(f"TAKE PROFIT: {current_trade_order}")
+                    self.__blogger.send_text_message(f"TAKE PROFIT: {candle.figi}")
+                    # close_order_id = \
+                    #     self.__close_position_by_figi(account_id, [candle.figi], strategies).get(candle.figi, None)
+                    # if close_order_id:
+                    #     trade_order = self.__today_trade_results.close_position(candle.figi, close_order_id)
+                    #     self.__blogger.close_position_message(trade_order)
 
-            if signal_new:
-                logger.info(f"New signal: {signal_new}")
+            if candle.time > current_figi_candle.time and \
+                    datetime.datetime.utcnow().replace(tzinfo=datetime.timezone.utc) <= signals_before_time:
+                signal_new = strategy.analyze_candles(
+                    [candle_to_historiccandle(current_figi_candle)]
+                )
 
-                if self.__today_trade_results.get_current_trade_order(candle.figi):
-                    logger.info(f"New signal has been skipped. Previous signal is still alive.")
-                elif not self.__market_data_service.is_stock_ready_for_trading(candle.figi):
-                    logger.info(f"New signal has been skipped. Stock isn't ready for trading")
-                else:
-                    available_lots = self.__open_position_lots_count(
-                        account_id,
-                        strategies[candle.figi].settings.max_lots_per_order,
-                        quotation_to_decimal(candle.close),
-                        strategies[candle.figi].settings.lot_size
-                    )
+                if signal_new:
+                    logger.info(f"New signal: {signal_new}")
 
-                    logger.debug(f"Available lots: {available_lots}")
-                    if available_lots:
-                        open_order_id = self.__order_service.post_market_order(
-                            account_id=account_id,
-                            figi=candle.figi,
-                            count_lots=available_lots,
-                            is_buy=(signal_new.signal_type == SignalType.LONG)
-                        )
-                        open_position = self.__today_trade_results.open_position(
-                            candle.figi,
-                            open_order_id,
-                            signal_new
-                        )
-                        self.__blogger.open_position_message(open_position)
-                        logger.info(f"Open position: {open_position}")
+                    if self.__today_trade_results.get_current_trade_order(candle.figi):
+                        logger.info(f"New signal has been skipped. Previous signal is still alive.")
+                    elif not self.__market_data_service.is_stock_ready_for_trading(candle.figi):
+                        logger.info(f"New signal has been skipped. Stock isn't ready for trading")
                     else:
-                        logger.info(f"New signal has been skipped. No available money")
+                        available_lots = self.__open_position_lots_count(
+                            account_id,
+                            strategy.settings.max_lots_per_order,
+                            quotation_to_decimal(candle.close),
+                            strategy.settings.lot_size
+                        )
 
-        current_candles[candle.figi] = candle
+                        logger.debug(f"Available lots: {available_lots}")
+                        if available_lots:
+                            # open_order_id = self.__order_service.post_market_order(
+                            #     account_id=account_id,
+                            #     figi=candle.figi,
+                            #     count_lots=available_lots,
+                            #     is_buy=(signal_new.signal_type == SignalType.LONG)
+                            # )
+                            # open_position = self.__today_trade_results.open_position(
+                            #     candle.figi,
+                            #     open_order_id,
+                            #     signal_new
+                            # )
+                            # self.__blogger.open_position_message(open_position)
+                            # logger.info(f"Open position: {open_position}")
+                            self.__blogger.send_text_message(f"Open position: {candle.figi}")
+                        else:
+                            logger.info(f"New signal has been skipped. No available money")
+
+            current_candles[candle.figi] = candle
 
     def __summary_today_trade_results(
             self,
@@ -370,6 +375,9 @@ class Trader:
                 # refresh information by latest info
                 strategy.update_lot_count(share_settings.lot)
                 strategy.update_short_status(share_settings.short_enabled_flag)
+
+                if strategy.settings.figi not in today_trade_strategy.keys():
+                    today_trade_strategy[strategy.settings.figi] = []
 
                 today_trade_strategy[strategy.settings.figi].append(strategy)
 
